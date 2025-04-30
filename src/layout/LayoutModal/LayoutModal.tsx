ce88a815ee0bcced3462f403b06afde57ee1d0ec
Natalia Grishina <tata@MacBook-Air-Natalia.local>
2025-04-30 11:53:31 +0300

    добавлена фокусировка внутри модального окна

--- src/layout/LayoutModal/LayoutModal.tsx
+++ src/layout/LayoutModal/LayoutModal.tsx
@@ -2,7 +2,7 @@ import { X } from "lucide-react";
 import Button from "../../components/Button/Button";
 import styles from "./layout-modal.module.scss";
 import { useLocation, useNavigate } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function LayoutModal({
 	title,
@@ -15,6 +15,7 @@ function LayoutModal({
 	const location = useLocation();
 	const backgroundPath = location.state?.backgroundPath || "/";
 	const [isClosing, setIsClosing] = useState(false);
+	const modalRef = useRef<HTMLDialogElement>(null);
 
 	function handleClose() {
 		setIsClosing(true);
@@ -23,10 +24,44 @@ function LayoutModal({
 		}, 300);
 	}
 
+	const trapFocus = (e: KeyboardEvent) => {
+		if (!modalRef.current) return;
+
+		const focusableElements = modalRef.current.querySelectorAll(
+			'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])'
+		);
+		const firstElement = focusableElements[0] as HTMLElement;
+		const lastElement = focusableElements[
+			focusableElements.length - 1
+		] as HTMLElement;
+
+		if (e.key === "Tab") {
+			if (e.shiftKey) {
+				if (document.activeElement === firstElement) {
+					lastElement.focus();
+					e.preventDefault();
+				}
+			} else {
+				if (document.activeElement === lastElement) {
+					firstElement.focus();
+					e.preventDefault();
+				}
+			}
+		}
+	};
+
 	useEffect(() => {
 		document.body.style.overflow = "hidden";
+
+		if (modalRef.current) {
+			modalRef.current.focus();
+		}
+
+		document.addEventListener("keydown", trapFocus);
+
 		return () => {
 			document.body.style.overflow = "auto";
+			document.removeEventListener("keydown", trapFocus);
 		};
 	}, []);
 
@@ -36,13 +71,17 @@ function LayoutModal({
 			onClick={handleClose}
 		>
 			<dialog
+				ref={modalRef}
 				open
 				className={styles["modal"]}
 				onClick={(e) => e.stopPropagation()}
+				tabIndex={-1}
+				aria-modal="true"
+				aria-labelledby="modal-title"
 			>
 				<div className={styles["content"]}>
 					<div className={styles["header"]}>
-						<p>{title}</p>
+						<p id="modal-title">{title}</p>
 						<Button
 							title="Закрыть"
 							aria-label="Закрыть модальное окно"
